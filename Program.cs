21e2851a48040732a69f5a4c5cb9bfbab4b6a3d7
Pasa02001 <54437734+Pasa02001@users.noreply.github.com>
2020-01-28 13:36:57 +0100

    InserionSort

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            int ammount = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[ammount];
+            Random random = new Random();
+            int x = 0;
+
+            while (x < ammount)
+            {
+                array[x] = random.Next(1, 100);
+                Console.Write(array[x]+" ");
+                x++;
+            }
+            sw.Start();
+            int a = 1;
+            int b = a;
+            int temp = 0;
+
+            while (a < array.Length)
+            {
+                b = a;
+                while (b > 0 && array[b-1] > array[b])
+                {
+                    temp = array[b];
+                    array[b] = array[b-1];
+                    array[b - 1] = temp;
+                    b--;
+                }
+                a++;
+            }
+            sw.Stop();
+
+            for (int i = 0; i<array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("insertion sort tog={0} sekunder", sw.Elapsed);
+
+        } 
+    }
+}
